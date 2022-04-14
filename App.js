e91f6e33b7f1d1e61db3b2e36a3b063b86c3fd54
Shane Geary <shane.geary@glowstik.com>
2022-04-14 10:32:14 -0600

    Make it better

---
+++ App.js
@@ -0,0 +1,38 @@
+//MONGODB PASSWORD: R2PcHN95RipGumm2
+//Connection: mongodb+srv://shane:<password>@graphqlcluster.qj6bf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+//username: shane
+
+
+const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+
+const schema = require('./server/schema/Schema')
+// const schema = require('./schema/TypesSchema')
+
+const App = express()
+const port = process.env.PORT || 4000
+
+App.use(cors())
+
+App.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}))
+
+mongoose.connect(`mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphqlcluster.qj6bf.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`
+,{useNewUrlParser: true, useUnifiedTopology: true}
+).then(() => {
+    App.listen({port: port}, () => { 
+        console.log(process.env.mongoUserName)
+        //localhost:4000
+        console.log('listening for requests on my awesome port 4000');
+    
+    })
+}).catch((e) => {
+    console.log(process.env.mongoUserName)
+    return console.log('Error :' + e)
+})
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # graphql-project
-# graphql-project
